7f5090719e6f5a4a79b4b0676b5820fe306b4ef0
邱達儒 <88436875+QiuDaru@users.noreply.github.com>
2021-11-04 19:48:34 +0800

    Add files via upload

--- 邱達儒/untitled0.py
+++ 邱達儒/untitled0.py
@@ -17,10 +17,8 @@ screen = pygame.display.set_mode([500, 500])
 
 w = 250 #y
 s = 250
-x = 250
-y = 250
-ColorR = 0 #0~255
-ColorG = 0 #0~255
+Colorr = 40 
+Colorg = 40 
 surfX = 20
 surfY = 20
 circle_size = 15
@@ -50,21 +48,17 @@ while running:
             if event.key == pygame.K_d:#右鍵改變
                     s = s + 10 
             if event.key == pygame.K_UP:#上鍵改變
-                    ColorR = ColorR - 50
-                    ColorG = ColorG - 50
+                    Colorr = Colorr - 25
             if event.key == pygame.K_DOWN:#下鍵改變
-                    ColorR = ColorR + 50
-                    ColorG = ColorG + 50
+                    Colorr = Colorr + 25
             if event.key == pygame.K_RIGHT: #右箭頭
-                    x = x + 10
+                    Colorg = Colorg + 25
             if event.key == pygame.K_LEFT: #左箭頭
-                    x = x - 10
+                    Colorg = Colorg - 25
             if event.key == pygame.K_v:
                     circle_size = circle_size - 10
-                    y = y - 10
             if event.key == pygame.K_c:
                     circle_size = circle_size + 10
-                    y = y + 10
     #============畫布區塊============    
 
     #畫布的顏色
@@ -81,28 +75,18 @@ while running:
         w = 500
     if s >= 500:
         s = 500
-    
-    if x >= 500:
-        x = 500
-    if y >=500:
-        y = 500
-    
-    if x <= 0 :
-        x = 0
-    if y <= 0:
-        y = 0
+    if Colorr >= 255:
+        Colorr = 255
+    if Colorr <= 0:
+        Colorr = 0
+    if Colorg >= 255:
+        Colorg = 255
+    if Colorg <= 0:
+        Colorg = 0
         
-    if ColorR <= 0:
-        ColorR = 0
-    if ColorR >= 255:
-        ColorR = 255
-    if ColorG <= 0:
-        ColorG = 0
-    if ColorG >= 255:
-        ColorG = 255
-    # pygame.draw.circle(畫布, 顏色, (x坐標, y坐標), 半徑, 線寬)
-    pygame.draw.circle(screen, (ColorR, 0, 255), (s, w), circle_size, 1)
-    pygame.draw.circle(screen, (0, 255, ColorG), (x, y), circle_size + 10)           
+    
+    pygame.draw.circle(screen,(Colorr, Colorg, 255), (s, w), circle_size)
+           
     #============畫布區塊============
 
     #將所有圖形顯示在畫布上
