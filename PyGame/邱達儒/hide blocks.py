dff545390046ac71f586c862983d35dca3a0ea51
pecu <pecu610@gmail.com>
2022-08-03 17:34:28 +0800

    update dr

--- PyGame/邱達儒/hide blocks.py
+++ PyGame/邱達儒/hide blocks.py
@@ -41,41 +41,20 @@ while running:
         Y1=500
         X1=random.randrange(10, 490)
    
-        
-    
-       
-                           
-
-    # Did the user click the window close button?
-
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-
             running = False
-            move =  False
-    while move :  
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 X = X + 10
             if event.key == pygame.K_LEFT:
-                X = X - 10
-      
+                X = X - 10                
                 
-                
-
     # Fill the background with white
 
     screen.fill((255, 255, 255))
 
-
     # Draw a solid blue circle in the center
-
-    
- 
-
-
     
     surf = pygame.Surface((40, 20))
     surf.fill((0,0,0))
@@ -85,12 +64,9 @@ while running:
 
     pygame.draw.circle(screen, (0, 0, 255), (X, Y), 10)
 
-
     # Flip the display
 
     pygame.display.flip()
 
-
 # Done! Time to quit.
-
 pygame.quit()
